12cadb12cfe44ccd612cd7a09d2c1b46eaf127cd
Thyago Coelho <thystockler@gmail.com>
2022-05-19 17:27:12 -0300

    chore: added crud template

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,25 +1,35 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
 
 export default class UsersController {
 
-    public async store({request, response}: HttpContextContract) {
-        const body = request.body()
+    public async store({request}: HttpContextContract) {
+        const user = request.only(['name','email','password','cnpj'])
+        
+        return await User.create(user);
 
-        return {
-            body,
-        }
     }
 
     public async index() {
-        return [
-            {
-                id: 1,
-                teste: 'teste'
-            },
-            {
-                id: 2,
-                teste: 'teste'
-            }
-        ]
+        return await User.all();
     }
+
+    public async show({ params }: HttpContextContract) {
+        return await User.find(params.id);
+    }
+
+    public async update({ params, request }: HttpContextContract) {
+        const course = await User.findOrFail(params.id);
+
+        const dataToUpdate = request.only(['name', 'description', 'url', 'price']);
+    
+        course.merge(dataToUpdate);
+    
+        await course.save();
+    
+        return course;
+    }
+
+    public async delete(id: number) {}
+
 }
